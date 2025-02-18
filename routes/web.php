d507fb924dbe5ad5a95bdb0a7c90dc6c9c44faaf
Noveryan Putra <noveryanpp@gmail.com>
2025-02-18 09:50:56 +0700

    crud table modal

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/post', function () {
+        return view('livewire/post');
+    })->name('post');
 });
